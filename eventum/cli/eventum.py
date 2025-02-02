83a6250053bbd1b3725654ef7a55a450e8e0c3d9
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-02-02 22:19:54 +0300

    Implement eventum run cli command

--- eventum/cli/eventum.py
+++ eventum/cli/eventum.py
@@ -1,48 +1,72 @@
-import click
-from pydanclick import from_pydantic
-from pydantic import ValidationError  # type: ignore[import-untyped]
+import os
+from io import TextIOWrapper
 
+import click
+import yaml
+from flatten_dict import unflatten
+from pydantic import ValidationError
+
+from eventum.cli.pydantic.converter import from_model
+from eventum.core.main import App, AppError
 from eventum.core.models.parameters.generator import GeneratorParameters
+from eventum.core.models.settings import Settings
 from eventum.utils.validation_prettier import prettify_validation_errors
 
 
 @click.group('eventum')
-def _cli():
+def cli():
     """Events generation platform."""
     pass
 
 
-@_cli.command
+@cli.command
 @click.option(
     '-c', '--config',
     required=True,
-    help='Path to main configuration file'
+    help='Path to main configuration file',
+    type=click.File()
 )
-def run(config: str) -> None:
+def run(config: TextIOWrapper) -> None:
     """Run application with all defined generators."""
-    print(config)
-
-
-@_cli.command
-@from_pydantic(
-    GeneratorParameters,
-    parse_docstring=True,
-    docstring_style='numpy'
-)
-def generate(generator_parameters: GeneratorParameters) -> None:
-    """Generate events using single generator."""
-    pass
-
-
-def cli():
     try:
-        _cli()
-    except ValidationError as e:
+        data = yaml.load(config, Loader=yaml.SafeLoader)
+    except yaml.error.YAMLError as e:
         click.echo(
-            'Error: Parameters validation failed: '
-            + prettify_validation_errors(e.errors())
+            f'Error: Failed to parse configuration: {str(e).capitalize()}',
+            err=True
         )
         exit(1)
+
+    data = unflatten(data, splitter='dot')
+
+    try:
+        settings = Settings.model_validate(data)
+    except ValidationError as e:
+        click.echo('Error: Failed to validate options:', err=True)
+        click.echo(
+            prettify_validation_errors(e.errors(), sep=os.linesep),
+            err=True
+        )
+        exit(1)
+
+    app = App(settings)
+
+    try:
+        app.start()
+    except AppError as e:
+        click.echo(
+            f'Error: App exited with error: {e} (see logs for more info)',
+            err=True
+        )
+        exit(1)
+
+
+@cli.command
+@from_model(GeneratorParameters)
+def generate(generator_parameters: GeneratorParameters) -> None:
+    """Generate events using single generator."""
+    print(generator_parameters)
+    # TODO: implement
 
 
 if __name__ == '__main__':
