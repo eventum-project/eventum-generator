e5cefc93f0ac3157a14aaae1ee9b7aa4853c310e
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-02-04 22:17:43 +0300

    Add termination handling to entrypoint

--- eventum/core/entrypoint.py
+++ eventum/core/entrypoint.py
@@ -1,7 +1,11 @@
+import atexit
 import os
+import signal
 import time
+from contextlib import suppress
 from multiprocessing.managers import DictProxy
 from threading import Event, Thread
+from typing import Iterable
 
 import structlog
 from setproctitle import getproctitle, setproctitle
@@ -94,31 +98,47 @@ def start(params: GeneratorParameters, metrics: DictProxy) -> None:
     stop_event = Event()
     gauge_thread = Thread(
         target=_gauge_metrics,
-        args=(gauge, metrics, stop_event, params.metrics_interval)
+        args=(gauge, metrics, stop_event, params.metrics_interval),
+        name='MetricsGaugingThread'
+    )
+
+    signal.signal(
+        signal.SIGTERM,
+        lambda _, __:  handle_termination(
+            events=[stop_event],
+            threads=[gauge_thread],
+            timeout=1,
+            exit_code=ExitCode.TERMINATED
+        )
     )
 
     init_time = round(time.time() - init_start_time, 3)
     logger.info('Initialization completed', seconds=init_time)
 
     logger.info('Starting execution', parameters=params.model_dump())
+
     gauge_thread.start()
     try:
         executor.execute()
     except ExecutionError as e:
         logger.error(str(e), **e.context)
-        exit(ExitCode.EXECUTION_ERROR)
+        exit_code = ExitCode.EXECUTION_ERROR
     except Exception as e:
         logger.exception(
             'Unexpected error occurred during execution',
             reason=str(e)
         )
-        exit(ExitCode.UNEXPECTED_ERROR)
-    finally:
-        stop_event.set()
-        gauge_thread.join()
+        exit_code = ExitCode.UNEXPECTED_ERROR
+    else:
+        logger.info('Finishing execution')
+        exit_code = ExitCode.SUCCESS
 
-    logger.info('Finishing execution')
-    exit(ExitCode.SUCCESS)
+    handle_termination(
+        events=[stop_event],
+        threads=[gauge_thread],
+        timeout=1,
+        exit_code=exit_code
+    )
 
 
 def _gauge_metrics(
@@ -151,8 +171,55 @@ def _gauge_metrics(
     if interval <= 0:
         raise ValueError('Interval must be greater than zero')
 
-    while not stop_event.is_set():
+    while not stop_event.wait(timeout=interval):
         metrics.update(gauge.gauge_metrics())
-        time.sleep(interval)
 
-    metrics.update(gauge.gauge_metrics())
+    # To the moment of stop event metrics proxy might be already closed
+    # But anyway we try to actualize eventual metrics if it's possible
+    with suppress(EOFError):
+        metrics.update(gauge.gauge_metrics())
+
+
+def handle_termination(
+    events: Iterable[Event],
+    threads: Iterable[Thread],
+    timeout: float = 1.0,
+    exit_code: ExitCode = ExitCode.SUCCESS
+) -> None:
+    """Handle termination of generator by setting all provided events,
+    joining all provided threads and running callbacks registered with
+    `atexit.register` in corresponding order within a provided timeout.
+
+    Parameters
+    ----------
+    events: Iterable[Event]
+        Events to set
+
+    threads: Iterable[Thread]
+        Threads to join
+
+    timeout : float, default=1.0
+        Maximum time (in seconds) for joining all threads
+
+    exit_code : ExitCode, default=ExitCode.SUCCESS
+        Code to exit with
+    """
+    for event in events:
+        event.set()
+
+    start_time = time.time()
+
+    for thread in threads:
+        spent = time.time() - start_time
+        available_time = max(timeout - spent, 0)
+        thread.join(available_time)
+
+        if thread.is_alive():
+            logger.warning(
+                'Thread was not joined in time',
+                thread_name=thread.name,
+                thread_id=thread.native_id
+            )
+
+    atexit._run_exitfuncs()
+    os._exit(exit_code)
