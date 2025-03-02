02394fb8b3dd0185c61ecaec07bbd95d0c5a24b2
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-02 15:04:13 +0300

    Update generator class to use threads instead of processes

--- eventum/core/generator.py
+++ eventum/core/generator.py
@@ -1,42 +1,91 @@
-from multiprocessing import Manager, Process
+from threading import Thread
 
-from eventum.core.entrypoint import start
+import structlog
+
+from eventum.core.config import ConfigurationLoadError, load
+from eventum.core.executor import Executor, ImproperlyConfiguredError
+from eventum.core.gauge import MetricsGauge
+from eventum.core.initializer import InitializationError, init_plugins
 from eventum.core.models.metrics import Metrics
 from eventum.core.models.parameters.generator import GeneratorParameters
+from eventum.utils.exceptions import ContextualException
+
+logger = structlog.stdlib.get_logger()
+
+
+class GeneratorInitializationError(ContextualException):
+    """Error during generator initialization."""
 
 
 class Generator:
-    """Generator process wrapper."""
+    """Thread-wrapped generator.
+
+    Raises
+    ------
+    GeneratorInitializationError
+        If any error occurs during generator initialization
+    """
 
     def __init__(self, params: GeneratorParameters) -> None:
         self._params = params
-        self._manager = Manager()
-        self._metrics = self._manager.dict()
-        self._process = Process(target=start, args=(params, self._metrics))
+
+        logger.info('Loading configuration', file_path=self._params.path)
+        try:
+            self._config = load(self._params.path, self._params.params)
+        except ConfigurationLoadError as e:
+            raise GeneratorInitializationError(str(e), context=e.context)
+
+        logger.info('Initializing plugins')
+        try:
+            self._plugins = init_plugins(
+                input=self._config.input,
+                event=self._config.event,
+                output=self._config.output,
+                params=self._params
+            )
+        except InitializationError as e:
+            raise GeneratorInitializationError(str(e), context=e.context)
+
+        logger.info('Initializing plugins executor')
+        try:
+            self._executor = Executor(
+                input=self._plugins.input,
+                event=self._plugins.event,
+                output=self._plugins.output,
+                params=self._params
+            )
+        except ImproperlyConfiguredError as e:
+            raise GeneratorInitializationError(str(e), context=e.context)
+
+        self._gauge = MetricsGauge(
+            input=self._plugins.input,
+            event=self._plugins.event,
+            output=self._plugins.output,
+            params=self._params
+        )
+
+        self._thread = Thread(target=self._executor.execute)
 
     def start(self) -> None:
         """Start generator in subprocess."""
         if self.is_running:
             return
 
-        self._process.start()
+        self._thread.start()
 
     def stop(self) -> None:
-        """Stop generator."""
+        """Stop generator with joining underlying thread. All possible
+        exceptions are propagated.
+        """
         if not self.is_running:
             return
 
-        self._process.terminate()
-
-    def force_stop(self) -> None:
-        """Stop generator using kill signal."""
-        if not self.is_running:
-            return
-
-        self._process.kill()
+        self._executor.request_stop()
+        self._thread.join()
 
     def join(self, timeout: float | None = None) -> bool:
-        """Wait until generator terminates.
+        """Wait until generator terminates. All possible exceptions are
+        propagated.
 
         Parameters
         ----------
@@ -52,11 +101,11 @@ class Generator:
         if not self.is_running:
             return True
 
-        self._process.join(timeout)
+        self._thread.join(timeout)
 
         return not self.is_running
 
-    def get_metrics(self) -> Metrics | None:
+    def get_metrics(self) -> Metrics:
         """Get generator metrics if available.
 
         Returns
@@ -64,17 +113,9 @@ class Generator:
         Metrics | None
             Generator metrics
         """
-        if self._metrics:
-            return Metrics(**self._metrics)     # type: ignore[typeddict-item]
-
-        return None
+        return self._gauge.gauge_metrics()
 
     @property
     def is_running(self) -> bool:
         """Whether the generator is running."""
-        return self._process.is_alive()
-
-    @property
-    def exit_code(self) -> int | None:
-        """Exit code of generator process."""
-        return self._process.exitcode
+        return self._thread.is_alive()
