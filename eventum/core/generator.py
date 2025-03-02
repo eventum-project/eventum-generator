f6e9c6373df00f04310c893b8b9dc1f3dc326b53
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-02 16:25:45 +0300

    Add handling exceptions inside the generator

--- eventum/core/generator.py
+++ eventum/core/generator.py
@@ -1,11 +1,15 @@
-from threading import Thread
+import os
+import time
+from threading import Thread, get_native_id
 
 import structlog
 
-from eventum.core.config import load
-from eventum.core.executor import Executor
+from eventum.core.config import ConfigurationLoadError, load
+from eventum.core.executor import (ExecutionError, Executor,
+                                   ImproperlyConfiguredError)
 from eventum.core.gauge import MetricsGauge
-from eventum.core.initializer import InitializedPlugins, init_plugins
+from eventum.core.initializer import (InitializationError, InitializedPlugins,
+                                      init_plugins)
 from eventum.core.models.config import GeneratorConfig
 from eventum.core.models.metrics import Metrics
 from eventum.core.models.parameters.generator import GeneratorParameters
@@ -41,24 +45,65 @@ class Generator:
         ExecutionError
             If any error occurs during execution
         """
+        # All exceptions are logged before propagated because we want to
+        # inform user about potential errors in logs as soon as possible
+        logger.info(
+            'Generator is started',
+            process_id=os.getpid(),
+            thread_id=get_native_id()
+        )
+
+        init_start_time = time.monotonic()
+
         logger.info('Loading configuration', file_path=self._params.path)
-        self._config = load(self._params.path, self._params.params)
+        try:
+            self._config = load(self._params.path, self._params.params)
+        except ConfigurationLoadError as e:
+            logger.error(str(e), **e.context)
+            raise e
+        except Exception as e:
+            logger.exception(
+                'Unexpected error occurred during loading config',
+                reason=str(e),
+                file_path=self._params.path
+            )
+            raise e
 
         logger.info('Initializing plugins')
-        self._plugins = init_plugins(
-            input=self._config.input,
-            event=self._config.event,
-            output=self._config.output,
-            params=self._params
-        )
+        try:
+            self._plugins = init_plugins(
+                input=self._config.input,
+                event=self._config.event,
+                output=self._config.output,
+                params=self._params
+            )
+        except InitializationError as e:
+            logger.error(str(e), **e.context)
+            raise e
+        except Exception as e:
+            logger.exception(
+                'Unexpected error occurred during initializing plugins',
+                reason=str(e)
+            )
+            raise e
 
         logger.info('Initializing plugins executor')
-        self._executor = Executor(
-            input=self._plugins.input,
-            event=self._plugins.event,
-            output=self._plugins.output,
-            params=self._params
-        )
+        try:
+            self._executor = Executor(
+                input=self._plugins.input,
+                event=self._plugins.event,
+                output=self._plugins.output,
+                params=self._params
+            )
+        except ImproperlyConfiguredError as e:
+            logger.error(str(e), **e.context)
+            raise e
+        except Exception as e:
+            logger.exception(
+                'Unexpected error occurred during initializing',
+                reason=str(e)
+            )
+            raise e
 
         self._gauge = MetricsGauge(
             input=self._plugins.input,
@@ -67,7 +112,22 @@ class Generator:
             params=self._params
         )
 
-        self._executor.execute()
+        init_time = round(time.monotonic() - init_start_time, 3)
+        logger.info('Initialization completed', seconds=init_time)
+
+        logger.info('Starting execution', parameters=self._params)
+
+        try:
+            self._executor.execute()
+        except ExecutionError as e:
+            logger.error(str(e), **e.context)
+            raise e
+        except Exception as e:
+            logger.exception(
+                'Unexpected error occurred during execution',
+                reason=str(e)
+            )
+            raise e
 
     def start(self) -> None:
         """Start generator in separate thread."""
@@ -92,29 +152,27 @@ class Generator:
             raise RuntimeError('Generator is not started')
 
         self._executor.request_stop()
-        self._thread.join()
 
-    def join(self, timeout: float | None = None) -> bool:
+        try:
+            self._thread.join()
+        except Exception:
+            logger.error('Generator stopped with errors')
+        else:
+            logger.info('Generator stopped successfully')
+
+    def join(self) -> None:
         """Wait until generator terminates with propagating all
         possible exceptions.
-
-        Parameters
-        ----------
-        timeout : float | None, default=None
-            Timeout of generator joining
-
-        Returns
-        -------
-        bool
-            `True` if generator was joined in time, and `False` if
-            timeout is expired
         """
         if not self.is_running:
-            return True
+            return
 
-        self._thread.join(timeout)
-
-        return not self.is_running
+        try:
+            self._thread.join()
+        except Exception:
+            logger.error('Generator ended up with errors')
+        else:
+            logger.info('Generator ended up successfully')
 
     def get_metrics(self) -> Metrics:
         """Get generator metrics if available.
