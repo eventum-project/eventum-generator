fba6e4e60b6ab950d05108ec5d86209168dd7c29
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-09 14:05:34 +0300

    Format sample_reader with ruff

--- eventum/plugins/event/plugins/jinja/sample_reader.py
+++ eventum/plugins/event/plugins/jinja/sample_reader.py
@@ -1,17 +1,23 @@
-from collections.abc import Iterable
-from typing import Any, Callable
+"""Definition of sample reader that provides unified interface for
+accessing samples of different types.
+"""
+
+from collections.abc import Callable, Iterable
+from typing import Any
 
 import tablib  # type: ignore[import-untyped]
 
-from eventum.plugins.event.plugins.jinja.config import (CSVSampleConfig,
-                                                        ItemsSampleConfig,
-                                                        JSONSampleConfig,
-                                                        SampleConfig,
-                                                        SampleType)
-from eventum.utils.exceptions import ContextualException
+from eventum.exceptions import ContextualError
+from eventum.plugins.event.plugins.jinja.config import (
+    CSVSampleConfig,
+    ItemsSampleConfig,
+    JSONSampleConfig,
+    SampleConfig,
+    SampleType,
+)
 
 
-class SampleLoadError(ContextualException):
+class SampleLoadError(ContextualError):
     """Failed to load sample."""
 
 
@@ -19,13 +25,21 @@ class Sample:
     """Immutable sample."""
 
     def __init__(self, dataset: tablib.Dataset) -> None:
+        """Initialize sample.
+
+        Parameters
+        ----------
+        dataset : tablib.Dataset
+            Sample data
+
+        """
         self._dataset = dataset
 
     def __len__(self) -> int:
-        return self._dataset.__len__()
+        return len(self._dataset)
 
-    def __getitem__(self, key: Any) -> list | tuple:
-        return self._dataset.__getitem__(key)
+    def __getitem__(self, key: Any) -> list | tuple:  # noqa: ANN401
+        return self._dataset[key]
 
 
 def _load_items_sample(config: ItemsSampleConfig) -> Sample:
@@ -40,6 +54,7 @@ def _load_items_sample(config: ItemsSampleConfig) -> Sample:
     -------
     Sample
         Loaded sample
+
     """
     data = tablib.Dataset()
 
@@ -51,7 +66,7 @@ def _load_items_sample(config: ItemsSampleConfig) -> Sample:
     if isinstance(first_row, Iterable) and not isinstance(first_row, str):
         data.extend(config.source)
     else:
-        data.extend((item, ) for item in config.source)
+        data.extend((item,) for item in config.source)
 
     return Sample(data)
 
@@ -73,6 +88,7 @@ def _load_csv_sample(config: CSVSampleConfig) -> Sample:
     ------
     Exception
         If some error occurs during sample loading
+
     """
     data = tablib.Dataset()
     with open(config.source) as f:
@@ -80,7 +96,7 @@ def _load_csv_sample(config: CSVSampleConfig) -> Sample:
             in_stream=f,
             format='csv',
             headers=config.header,
-            delimiter=config.delimiter
+            delimiter=config.delimiter,
         )
         return Sample(data)
 
@@ -102,18 +118,19 @@ def _load_json_sample(config: JSONSampleConfig) -> Sample:
     ------
     Exception
         If some error occurs during sample loading
+
     """
     data = tablib.Dataset()
     with open(config.source) as f:
         data.load(
             in_stream=f,
-            format='json'
+            format='json',
         )
         return Sample(data)
 
 
 def _get_sample_loader(
-    sample_type: SampleType
+    sample_type: SampleType,
 ) -> Callable[[SampleConfig], Sample]:
     """Get sample loader for specified sample type.
 
@@ -131,43 +148,48 @@ def _get_sample_loader(
     ------
     ValueError
         If no loader is registered for specified sample type
+
     """
     try:
         return {
             SampleType.ITEMS: _load_items_sample,
             SampleType.CSV: _load_csv_sample,
-            SampleType.JSON: _load_json_sample
+            SampleType.JSON: _load_json_sample,
         }[sample_type]  # type: ignore[return-value]
     except KeyError as e:
-        raise ValueError(f'No loader is registered for sample type "{e}"')
+        msg = f'No loader is available for sample type "{e}"'
+        raise ValueError(msg) from e
 
 
 class SampleReader:
-    """Sample reader.
-
-    Parameters
-    ----------
-    config : dict[str, SampleConfig]
-        Sample names to their configurations mapping
-
-    Raises
-    ------
-    SampleLoadError
-        If some error occurs during samples loading
-    """
+    """Sample reader."""
 
     def __init__(self, config: dict[str, SampleConfig]) -> None:
+        """Initialize sample reader.
+
+        Parameters
+        ----------
+        config : dict[str, SampleConfig]
+            Sample names to their configurations mapping
+
+        Raises
+        ------
+        SampleLoadError
+            If some error occurs during samples loading
+
+        """
         self._samples = self._load_samples(config)
 
     def __getitem__(self, name: str) -> Sample:
         try:
             return self._samples[name]
         except KeyError as e:
-            raise KeyError(f'No such sample "{e}"') from None
+            msg = f'No such sample "{e}"'
+            raise KeyError(msg) from None
 
     def _load_samples(
         self,
-        config: dict[str, SampleConfig]
+        config: dict[str, SampleConfig],
     ) -> dict[str, Sample]:
         """Load samples specified in config.
 
@@ -185,17 +207,19 @@ class SampleReader:
         ------
         SampleLoadError
             If some error occurs during samples loading
+
         """
-        samples: dict[str, Sample] = dict()
+        samples: dict[str, Sample] = {}
 
         for name, sample_config in config.items():
             loader = _get_sample_loader(sample_config.root.type)
             try:
                 sample = loader(sample_config.root)  # type: ignore[arg-type]
-            except Exception as e:
+            except Exception as e:  # noqa: BLE001
+                msg = 'Failed to load sample'
                 raise SampleLoadError(
-                    'Failed to load sample',
-                    context=dict(sample_alias=name, reason=str(e))
+                    msg,
+                    context={'sample_alias': name, 'reason': str(e)},
                 ) from None
 
             samples[name] = sample
