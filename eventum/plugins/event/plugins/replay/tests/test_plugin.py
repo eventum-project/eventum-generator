2fdb11d11e201b1e9421f3d02b86589d6c6994f4
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-11 23:12:52 +0300

    Format tests for replay event plugin

--- eventum/plugins/event/plugins/replay/tests/test_plugin.py
+++ eventum/plugins/event/plugins/replay/tests/test_plugin.py
@@ -1,6 +1,5 @@
-# type: ignore
-import os
 from datetime import datetime
+from pathlib import Path
 
 import pytest
 
@@ -8,32 +7,24 @@ from eventum.plugins.event.plugins.replay.config import ReplayEventPluginConfig
 from eventum.plugins.event.plugins.replay.plugin import ReplayEventPlugin
 from eventum.plugins.exceptions import PluginConfigurationError
 
-STATIC_DIR = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)),
-    'static'
-)
+STATIC_DIR = Path(__file__).parent / 'static'
 
 
 def test_plugin_substitution_with_format():
     plugin = ReplayEventPlugin(
         config=ReplayEventPluginConfig(
-            path=os.path.join(STATIC_DIR, 'example'),
+            path=STATIC_DIR / 'example',
             timestamp_pattern=r'\[(?P<timestamp>.*?)\]',
-            timestamp_format='%Y'
+            timestamp_format='%Y',
         ),
-        params={'id': 1}
+        params={'id': 1},
     )
 
     events = []
     now = datetime.now().astimezone()
     for _ in range(10):
         events.extend(
-            plugin.produce(
-                params={
-                    'timestamp': now,
-                    'tags': ('a', )
-                }
-            )
+            plugin.produce(params={'timestamp': now, 'tags': ('a',)})
         )
 
     year = now.year
@@ -47,17 +38,17 @@ def test_plugin_substitution_with_format():
         f'10.0.0.1 - - [{year}] "PATCH /update HTTP/1.1" 204 64',
         f'127.0.0.1 - - [{year}] "OPTIONS /info HTTP/1.1" 200 16',
         f'192.168.1.1 - - [{year}] "HEAD /check HTTP/1.1" 200 8',
-        f'10.0.0.1 - - [{year}] "TRACE /trace HTTP/1.1" 500 128'
+        f'10.0.0.1 - - [{year}] "TRACE /trace HTTP/1.1" 500 128',
     ]
 
 
 def test_plugin_substitution_with_default_format():
     plugin = ReplayEventPlugin(
         config=ReplayEventPluginConfig(
-            path=os.path.join(STATIC_DIR, 'example'),
+            path=STATIC_DIR / 'example',
             timestamp_pattern=r'\[(?P<timestamp>.*?)\]',
         ),
-        params={'id': 1}
+        params={'id': 1},
     )
 
     events = []
@@ -65,12 +56,7 @@ def test_plugin_substitution_with_default_format():
     now = datetime.now().astimezone()
     for _ in range(10):
         events.extend(
-            plugin.produce(
-                params={
-                    'timestamp': now,
-                    'tags': ('a', )
-                }
-            )
+            plugin.produce(params={'timestamp': now, 'tags': ('a',)})
         )
 
     ts = now.isoformat()
@@ -84,51 +70,43 @@ def test_plugin_substitution_with_default_format():
         f'10.0.0.1 - - [{ts}] "PATCH /update HTTP/1.1" 204 64',
         f'127.0.0.1 - - [{ts}] "OPTIONS /info HTTP/1.1" 200 16',
         f'192.168.1.1 - - [{ts}] "HEAD /check HTTP/1.1" 200 8',
-        f'10.0.0.1 - - [{ts}] "TRACE /trace HTTP/1.1" 500 128'
+        f'10.0.0.1 - - [{ts}] "TRACE /trace HTTP/1.1" 500 128',
     ]
 
 
 def test_plugin_substitution_with_missing_group():
     plugin = ReplayEventPlugin(
         config=ReplayEventPluginConfig(
-            path=os.path.join(STATIC_DIR, 'example'),
+            path=STATIC_DIR / 'example',
             timestamp_pattern=r'\[(?P<ts>.*?)\]',
         ),
-        params={'id': 1}
+        params={'id': 1},
     )
 
     events = plugin.produce(
-        params={
-            'timestamp': datetime.now().astimezone(),
-            'tags': ('a', )
-        }
+        params={'timestamp': datetime.now().astimezone(), 'tags': ('a',)}
     )
 
     assert events == [
-        '127.0.0.1 - - [01/Dec/2023:12:34:56 +0000] "GET /index.html HTTP/1.1" 200 1024'    # noqa
+        '127.0.0.1 - - [01/Dec/2023:12:34:56 +0000] "GET /index.html HTTP/1.1" 200 1024'  # noqa
     ]
 
 
 def test_plugin_repeat():
     plugin = ReplayEventPlugin(
         config=ReplayEventPluginConfig(
-            path=os.path.join(STATIC_DIR, 'example'),
+            path=STATIC_DIR / 'example',
             timestamp_pattern=r'\[(?P<timestamp>.*?)\]',
-            repeat=True
+            repeat=True,
         ),
-        params={'id': 1}
+        params={'id': 1},
     )
 
     events = []
     now = datetime.now().astimezone()
     for _ in range(15):
         events.extend(
-            plugin.produce(
-                params={
-                    'timestamp': now,
-                    'tags': ('a', )
-                }
-            )
+            plugin.produce(params={'timestamp': now, 'tags': ('a',)})
         )
 
     ts = now.isoformat()
@@ -147,7 +125,7 @@ def test_plugin_repeat():
         f'127.0.0.1 - - [{ts}] "POST /form HTTP/1.1" 201 512',
         f'192.168.1.1 - - [{ts}] "GET /about.html HTTP/1.1" 200 2048',
         f'10.0.0.1 - - [{ts}] "DELETE /resource HTTP/1.1" 403 256',
-        f'127.0.0.1 - - [{ts}] "PUT /api/data HTTP/1.1" 500 0'
+        f'127.0.0.1 - - [{ts}] "PUT /api/data HTTP/1.1" 500 0',
     ]
 
 
@@ -155,10 +133,10 @@ def test_plugin_substitution_with_bad_regex():
     with pytest.raises(PluginConfigurationError):
         ReplayEventPlugin(
             config=ReplayEventPluginConfig(
-                path=os.path.join(STATIC_DIR, 'example'),
+                path=STATIC_DIR / 'example',
                 timestamp_pattern=r'\[(?P<timestamp>[.*?)\]',
             ),
-            params={'id': 1}
+            params={'id': 1},
         )
 
 
@@ -166,8 +144,8 @@ def test_plugin_missing_file():
     with pytest.raises(PluginConfigurationError):
         ReplayEventPlugin(
             config=ReplayEventPluginConfig(
-                path=os.path.join(STATIC_DIR, 'unexistent'),
+                path=STATIC_DIR / 'unexistent',
                 timestamp_pattern=r'\[(?P<timestamp>.*?)\]',
             ),
-            params={'id': 1}
+            params={'id': 1},
         )
