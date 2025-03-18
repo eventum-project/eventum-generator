a7d175e4e86e6acd7c0c0625e75bac80f4b41348
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-18 20:49:27 +0300

    Add exceptions handling in cron input plugin

--- eventum/plugins/input/plugins/cron/plugin.py
+++ eventum/plugins/input/plugins/cron/plugin.py
@@ -9,6 +9,7 @@ from numpy import datetime64
 from numpy.typing import NDArray
 
 from eventum.plugins.input.base.plugin import InputPlugin, InputPluginParams
+from eventum.plugins.input.exceptions import PluginGenerationError
 from eventum.plugins.input.normalizers import normalize_versatile_daterange
 from eventum.plugins.input.plugins.cron.config import CronInputPluginConfig
 
@@ -34,13 +35,21 @@ class CronInputPlugin(InputPlugin[CronInputPluginConfig, InputPluginParams]):
         skip_past: bool = True,
     ) -> Iterator[NDArray[datetime64]]:
         now = datetime.now().astimezone(self._timezone)
-        start, end = normalize_versatile_daterange(
-            start=self._config.start,
-            end=self._config.end,
-            timezone=self._timezone,
-            none_start='now',
-            none_end='max',
-        )
+
+        try:
+            start, end = normalize_versatile_daterange(
+                start=self._config.start,
+                end=self._config.end,
+                timezone=self._timezone,
+                none_start='now',
+                none_end='max',
+            )
+        except (ValueError, OverflowError) as e:
+            msg = 'Failed to normalize daterange'
+            raise PluginGenerationError(
+                msg,
+                context={'reason': str(e)},
+            ) from None
 
         self._logger.info(
             'Generating in range',
