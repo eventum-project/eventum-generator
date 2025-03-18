62cbb294cccb626fbcbdbc1f423c170ff381b088
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-18 21:43:51 +0300

    Add PluginExhaustedError to event plugin exceptions

--- eventum/plugins/event/exceptions.py
+++ eventum/plugins/event/exceptions.py
@@ -1,5 +1,11 @@
 """Event plugin exceptions."""
 
+from eventum.plugins.exceptions import PluginError
 
-class EventsExhaustedError(Exception):
+
+class PluginExhaustedError(Exception):
     """No more events can be produced by event plugin."""
+
+
+class PluginProduceError(PluginError):
+    """Event cannot be produced."""
