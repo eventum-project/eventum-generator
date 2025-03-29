cffe7685de19c66db563e1b01827d7692cfc3cf6
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-03-29 13:01:52 +0300

    style(cli): :art: Format `pydantic_converted` module

--- eventum/cli/pydantic_converter.py
+++ eventum/cli/pydantic_converter.py
@@ -1,14 +1,22 @@
+"""Converted of pydantic models to click options."""
+
 import functools
 import os
+import sys
+from collections.abc import Callable, Sequence
 from types import NoneType, UnionType
-from typing import Any, Callable, Literal, Sequence, get_args, get_origin
+from typing import Any, Literal, get_args, get_origin
 
 import click.types as click_types
 from caseconverter import snakecase  # type: ignore[import-untyped]
 from click import Command, Option, echo
 from flatten_dict import unflatten  # type: ignore[import-untyped]
-from griffe import (Docstring, DocstringAttribute, DocstringSectionAttributes,
-                    parse_numpy)
+from griffe import (
+    Docstring,
+    DocstringAttribute,
+    DocstringSectionAttributes,
+    parse_numpy,
+)
 from pydantic import BaseModel, ValidationError
 from pydantic.fields import FieldInfo
 from pydantic_core import ErrorDetails, PydanticUndefined
@@ -22,24 +30,26 @@ def _parse_docstring(model: type[BaseModel]) -> dict[str, str]:
     Parameters
     ----------
     model : type[BaseModel]
-        Model the documentation of which should be parsed
+        Model the documentation of which should be parsed.
 
     Returns
     -------
     dict[str, str]
         Map with attribute names in keys and their descriptions in
-        values
+        values.
+
     """
-    class_docs: list[str] = []
-    for cls in [model, *model.__bases__]:
+    class_docs = [
+        cls.__doc__
+        for cls in [model, *model.__bases__]
         if (
             issubclass(cls, BaseModel)
             and cls is not BaseModel
             and cls.__doc__ is not None
-        ):
-            class_docs.append(cls.__doc__)
+        )
+    ]
 
-    field_docs: dict[str, str] = dict()
+    field_docs: dict[str, str] = {}
 
     for doc in class_docs:
         docstring = parse_numpy(docstring=Docstring(doc))
@@ -62,19 +72,21 @@ def build_arg_name(fields_stack: list[str]) -> str:
     Parameters
     ----------
     fields_stack : list[str]
-        Field names stack
+        Field names stack.
 
     Returns
     -------
-    Argument name including all stack of field names
+    Argument name including all stack of field names.
 
     Raises
     ------
     ValueError
-        If fields stack is empty
+        If fields stack is empty.
+
     """
     if not fields_stack:
-        raise ValueError('At least one field must be in the stack')
+        msg = 'At least one field must be in the stack'
+        raise ValueError(msg)
 
     return '__'.join(fields_stack)
 
@@ -85,19 +97,21 @@ def build_option_name(fields_stack: list[str]) -> str:
     Parameters
     ----------
     fields_stack : list[str]
-        Field names stack
+        Field names stack.
 
     Returns
     -------
-    Option name including all stack of field names
+    Option name including all stack of field names.
 
     Raises
     ------
     ValueError
-        If fields stack is empty
+        If fields stack is empty.
+
     """
     if not fields_stack:
-        raise ValueError('At least one field must be in the stack')
+        msg = 'At least one field must be in the stack'
+        raise ValueError(msg)
 
     return '--' + '.'.join(fields_stack).replace('_', '-')
 
@@ -110,73 +124,74 @@ def build_object_from_args(**kwargs: Any) -> dict[str, Any]:
     Parameters
     ----------
     **kwargs : Any
-        Command options as keyword arguments
+        Command options as keyword arguments.
 
     Returns
     -------
     dict[str, Any]
-        Unflattened object that is built of arguments
+        Unflattened object that is built of arguments.
+
     """
     flat_object = {
-        key.replace('__', '.'): value
-        for key, value in kwargs.items()
+        key.replace('__', '.'): value for key, value in kwargs.items()
     }
     return unflatten(flat_object, splitter='dot')
 
 
 def _get_type_for_annotation(
-    annotation: type[Any] | None
+    annotation: type[Any] | None,
 ) -> type | click_types.ParamType | None:
     """Get click compatible type from annotation.
 
     Parameters
     ----------
     annotation : type[Any] | None
-        Annotation to convert to click type
+        Annotation to convert to click type.
 
     Returns
     -------
     type | click_types.ParamType | None
-        Python type or click parameter type
+        Python type or click parameter type.
+
     """
     origin_type = get_origin(annotation)
     type_args = get_args(annotation)
 
     if (
         origin_type is UnionType
-        and len(type_args) == 2
+        and len(type_args) == 2  # noqa: PLR2004
         and NoneType in type_args
     ):
         return next(arg for arg in type_args if arg is not NoneType)
-    elif origin_type is Literal:
+    if origin_type is Literal:
         return click_types.Choice(type_args)
-    else:
-        return annotation
+    return annotation
 
 
 def _create_option(
     decls: Sequence[str],
     description: str | None,
-    field_info: FieldInfo
+    field_info: FieldInfo,
 ) -> Option:
     """Create `click.Option` from pydantic `FieldInfo` object.
 
     Parameters
     ----------
     decls : Sequence[str]
-        Option declarations
+        Option declarations.
 
     description : str | None
         Option description that is used when description is not defined
-        in field info
+        in field info.
 
     field_info : FieldInfo
-        Pydantic field info
+        Pydantic field info.
 
     Returns
     -------
     click.Option
         Click option
+
     """
     default: Any | None = None
     show_default = False
@@ -203,28 +218,29 @@ def _create_option(
         default=default,
         required=required,
         help=description,
-        type=type
+        type=type,
     )
 
 
 def _convert_to_options(
     fields_stack: list[str],
-    model: type[BaseModel]
+    model: type[BaseModel],
 ) -> list[Option]:
     """Convert Pydantic model to list of Click options.
 
     Parameters
     ----------
     fields_stack : list[str]
-        Current stack of field names
+        Current stack of field names.
 
     model : type[BaseModel]
-        Pydantic model
+        Pydantic model.
 
     Returns
     -------
     list[Option]
-        List of click options
+        List of click options.
+
     """
     options: list[Option] = []
 
@@ -243,7 +259,7 @@ def _convert_to_options(
             option = _create_option(
                 decls=(option_name, arg_name),
                 description=field_docs.get(name, None),
-                field_info=info
+                field_info=info,
             )
             options.append(option)
 
@@ -254,12 +270,13 @@ def _convert_to_options(
 
 def add_options(
     _f_or_opts: Callable | list[Option],
-    options: list[Option] | None = None
+    options: list[Option] | None = None,
 ) -> Callable:
     """Add list of options to a function."""
     if callable(_f_or_opts):
         if options is None:
-            raise ValueError('No options specified')
+            msg = 'No options specified'
+            raise ValueError(msg)
 
         return _register_options(_f_or_opts, options)
 
@@ -271,7 +288,7 @@ def add_options(
 
 def _register_options(
     f: Callable,
-    options: list[Option]
+    options: list[Option],
 ) -> Callable:
     """Register options using Click internal mechanism."""
     ordered_options = reversed(options)
@@ -279,11 +296,11 @@ def _register_options(
     if isinstance(f, Command):
         f.params.extend(ordered_options)
     else:
-        if not hasattr(f, "__click_params__"):
-            f.__click_params__ = []     # type: ignore[attr-defined]
+        if not hasattr(f, '__click_params__'):
+            f.__click_params__ = []  # type: ignore[attr-defined]
 
-        f.__click_params__.extend(      # type: ignore[attr-defined]
-            ordered_options
+        f.__click_params__.extend(  # type: ignore[attr-defined]
+            ordered_options,
         )
 
     return f
@@ -296,15 +313,16 @@ def _patch_error_locations(errors: list[ErrorDetails]) -> None:
     Parameters
     ----------
     errors : list[ErrorDetails]
-        Errors list to patch in place
+        Errors list to patch in place.
+
     """
     for error in errors:
         new_locs: list[str | int] = []
         for i, level in enumerate(error['loc']):
             if isinstance(level, str):
-                level = level.replace('_', '-')
+                level = level.replace('_', '-')  # noqa: PLW2901
                 if i == 0:
-                    level = '--' + level
+                    level = '--' + level  # noqa: PLW2901
 
             new_locs.append(level)
 
@@ -314,7 +332,7 @@ def _patch_error_locations(errors: list[ErrorDetails]) -> None:
 def from_model(model: type[BaseModel]) -> Callable[[Callable], Callable]:
     """Decorator for adding options to click command from pydantic
     model.
-    """
+    """  # noqa: D401
     options = _convert_to_options(fields_stack=[], model=model)
     param_name = snakecase(model.__name__)
 
@@ -324,8 +342,8 @@ def from_model(model: type[BaseModel]) -> Callable[[Callable], Callable]:
         def wrapped(**kwargs: Any) -> Any:
             object = build_object_from_args(**kwargs)
 
-            model_kwargs: dict[str, Any] = dict()
-            other_kwargs: dict[str, Any] = dict()
+            model_kwargs: dict[str, Any] = {}
+            other_kwargs: dict[str, Any] = {}
             for k, v in object.items():
                 if k in model.__pydantic_fields__:
                     model_kwargs[k] = v
@@ -341,7 +359,7 @@ def from_model(model: type[BaseModel]) -> Callable[[Callable], Callable]:
                 echo('Error: Failed to validate options:')
                 echo(prettify_validation_errors(errors, sep=os.linesep))
 
-                exit(1)
+                sys.exit(1)
 
             other_kwargs[param_name] = validated
 
