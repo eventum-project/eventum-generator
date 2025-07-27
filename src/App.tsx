c9494dee8e9f28ce776e6fd7e3a6fa6580e33be1
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-07-27 10:25:02 +0300

    Fix import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 
-import AppRouter from '@/routes';
+import AppRouter from '@/routing';
 
 export default function App() {
   return (
