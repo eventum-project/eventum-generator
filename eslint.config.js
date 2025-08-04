bc623c4039fd956f46c8c9d1ba44a0c35ea6f4eb
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-08-04 22:48:22 +0300

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default tseslint.config({
     'unicorn/filename-case': 'off',
     'unicorn/prevent-abbreviations': 'off',
     'unicorn/no-null': 'off',
+    'sonarjs/void-use': 'off',
   },
   languageOptions: {
     ecmaVersion: 2020,
