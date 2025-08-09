9c5cf649403c4e4a48815a743569f23d7d6471ca
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-08-09 11:50:20 +0300

    feat(api): :sparkles: Add generator status endpoint

--- eventum/api/routes/generators.py
+++ eventum/api/routes/generators.py
@@ -1,6 +1,7 @@
 """Generator routes."""
 
 from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
 
 from eventum.api.dependencies import GeneratorManagerDep
 from eventum.app.manager import ManagingError
@@ -19,6 +20,59 @@ router = APIRouter(
 )
 def list_generators(generator_manager: GeneratorManagerDep) -> list[str]:
     return generator_manager.generator_ids
+
+
+class GeneratorStatus(BaseModel, frozen=True, extra='forbid'):
+    """Status of generator.
+
+    Attributes
+    ----------
+    is_initializing : bool
+        Whether the generator is initializing.
+
+    is_running : bool
+        Whether the generator is running.
+
+    is_ended_up : bool
+        Whether the generator has ended execution with or without
+        errors.
+
+    is_ended_up_successfully : bool
+        Whether the generator has ended execution successfully.
+
+    """
+
+    is_initializing: bool
+    is_running: bool
+    is_ended_up: bool
+    is_ended_up_successfully: bool
+
+
+@router.get(
+    '/{id}/status/',
+    description='Get generator status',
+    responses={
+        404: {'description': 'Generator with provided id is not found'},
+    },
+)
+def generator_status(
+    id: str,
+    generator_manager: GeneratorManagerDep,
+) -> GeneratorStatus:
+    try:
+        generator = generator_manager.get_generator(id)
+    except ManagingError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e),
+        ) from None
+
+    return GeneratorStatus(
+        is_initializing=generator.is_initializing,
+        is_running=generator.is_running,
+        is_ended_up=generator.is_ended_up,
+        is_ended_up_successfully=generator.is_ended_up_successfully,
+    )
 
 
 @router.post(
