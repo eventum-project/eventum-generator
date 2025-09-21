5eef04321a4c8a8dbba19036ed9391eeccf8b5ba
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-09-21 17:10:00 +0300

    test(api): :white_check_mark: Remove unused imports from tests

--- eventum/api/tests/test_main.py
+++ eventum/api/tests/test_main.py
@@ -1,11 +1,8 @@
 from unittest.mock import MagicMock
 
-import pytest
 from fastapi import FastAPI
 
-import eventum.api.routers as routes
 from eventum.api.main import build_api_app
-from eventum.utils.package_utils import get_subpackage_names
 
 
 def test_build_api_app():
@@ -24,4 +21,7 @@ def test_api_app_state():
     app = build_api_app(generator_manager=mock_manager, settings=mock_settings)
 
     assert app.state.generator_manager is mock_manager
+    assert app.state.settings is mock_settings
+    assert app.state.settings is mock_settings
+    assert app.state.settings is mock_settings
     assert app.state.settings is mock_settings
