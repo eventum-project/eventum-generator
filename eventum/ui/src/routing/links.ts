81105ff52c5c3fb8cc41462c43f7495d6115f091
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-10-05 15:35:26 +0300

    feat(ui): :wrench: Add file with external links

---
+++ eventum/ui/src/routing/links.ts
@@ -0,0 +1,5 @@
+export const GITHUB_ORGANIZATION = 'https://github.com/eventum-project';
+export const TG_COMMUNITY_GROUP = 'https://t.me/+5aw8dFk84W1jNTg6';
+export const DOCUMENTATION = 'https://eventum-project.github.io/website/';
+export const LICENSE =
+  'https://github.com/eventum-project/eventum-generator/blob/master/LICENSE';
