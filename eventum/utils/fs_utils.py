b0c1b3efb385906b5fcb3c6a1a7963c5cc34bba8
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-11-06 13:30:24 +0300

    feat(utils): :sparkles: Add `get_dir_last_modification_time` fs util

--- eventum/utils/fs_utils.py
+++ eventum/utils/fs_utils.py
@@ -23,3 +23,32 @@ def calculate_dir_size(path: Path) -> int:
 
     """
     return sum(f.stat().st_size for f in path.glob('**/*') if f.is_file())
+
+
+def get_dir_last_modification_time(path: Path) -> float:
+    """Get last modification time of all files inside the directory.
+
+    Parameters
+    ----------
+    path : Path
+        Path to directory.
+
+    Returns
+    -------
+    float
+        Unix timestamp.
+
+    Raises
+    ------
+    OSError
+        If some of the resources cannot be accessed.
+
+    """
+    latest = path.stat().st_mtime
+
+    for item in path.rglob('*'):
+        if item.is_file():
+            mtime = item.stat().st_mtime
+            latest = max(latest, mtime)
+
+    return latest
