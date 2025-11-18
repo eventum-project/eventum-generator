ad4941701b8f032b2a4773c003a905e7e438251b
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-11-18 23:15:49 +0300

    feat(ui): :sparkles: Add routes and hooks to preview generated timestamps

--- eventum/ui/src/api/hooks/usePreview.ts
+++ eventum/ui/src/api/hooks/usePreview.ts
@@ -1,7 +1,39 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { normalizeVersatileDatetime } from '../routes/preview';
+import { InputPluginsNamedConfig } from '../routes/generator-configs/schemas';
+import {
+  generateTimestamps,
+  normalizeVersatileDatetime,
+} from '../routes/preview';
 import { VersatileDatetimeParametersBody } from '../routes/preview/schemas';
+
+export function useGenerateTimestampMutation() {
+  return useMutation({
+    mutationFn: ({
+      name,
+      size,
+      skipPast,
+      timezone,
+      span,
+      inputPluginsConfig,
+    }: {
+      name: string;
+      size: number;
+      skipPast: boolean;
+      timezone: string;
+      span: string;
+      inputPluginsConfig: InputPluginsNamedConfig;
+    }) =>
+      generateTimestamps(
+        name,
+        size,
+        skipPast,
+        timezone,
+        span,
+        inputPluginsConfig
+      ),
+  });
+}
 
 export function useNormalizedVersatileDatetimeMutation() {
   return useMutation({
--- eventum/ui/src/api/routes/preview/schemas.ts
+++ eventum/ui/src/api/routes/preview/schemas.ts
@@ -1,5 +1,11 @@
 import z from 'zod';
 
+export const AggregatedTimestampsSchema = z.object({
+  span_edges: z.array(z.string()),
+  span_counts: z.record(z.int(), z.array(z.int())),
+});
+export type AggregatedTimestamps = z.infer<typeof AggregatedTimestampsSchema>;
+
 export const VersatileDatetimeParametersBodySchema = z.object({
   value: z.string().nullable().optional(),
   timezone: z.string().min(1),
