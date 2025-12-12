9f55c8d67f4cfe6ea73b919294a7c211613340ba
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-12-12 23:32:08 +0300

    feat(ui): :sparkles: Add `TemplateEventPluginWorkspace` component

--- eventum/ui/src/pages/ProjectPage/EventPluginTab/Workspace/TemplateEventPluginWorkspace/index.tsx
+++ eventum/ui/src/pages/ProjectPage/EventPluginTab/Workspace/TemplateEventPluginWorkspace/index.tsx
@@ -1,7 +1,24 @@
+import { Box, Stack, Tabs } from '@mantine/core';
 import { FC } from 'react';
 
-import { TemplateEditor } from './TemplateEditor';
+import { EditorTab } from './EditorTab';
 
 export const TemplateEventPluginWorkspace: FC = () => {
-  return <TemplateEditor />;
+  return (
+    <Stack>
+      <Tabs defaultValue="editor">
+        <Tabs.List>
+          <Tabs.Tab value="editor">Editor</Tabs.Tab>
+          <Tabs.Tab value="debugger">Debugger</Tabs.Tab>
+        </Tabs.List>
+
+        <Box mt="md">
+          <Tabs.Panel value="editor">
+            <EditorTab />
+          </Tabs.Panel>
+          <Tabs.Panel value="debugger">debugger</Tabs.Panel>
+        </Box>
+      </Tabs>
+    </Stack>
+  );
 };
