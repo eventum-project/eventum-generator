3eb49096f179c1a2b859094ffb8b2777d55bb688
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2025-12-24 21:55:50 +0300

    feat(ui): :sparkles: Extract write modes to const in file output plugin config schema

--- eventum/ui/src/api/routes/generator-configs/schemas/plugins/output/configs/file.ts
+++ eventum/ui/src/api/routes/generator-configs/schemas/plugins/output/configs/file.ts
@@ -3,13 +3,15 @@ import z from 'zod';
 import { ENCODINGS } from '../../../encodings';
 import { BaseOutputPluginConfigSchema } from '../base-config';
 
+export const WRITE_MODES = ['append', 'overwrite'] as const;
+
 export const FileOutputPluginConfigSchema = BaseOutputPluginConfigSchema.extend(
   {
     path: z.string().min(1),
     flush_interval: z.number().gte(0).optional(),
     cleanup_interval: z.number().gte(1).optional(),
     file_mode: z.number().int().gte(0).lte(7777).optional(),
-    write_mode: z.enum(['append', 'overwrite']).optional(),
+    write_mode: z.enum(WRITE_MODES).optional(),
     encoding: z.enum(ENCODINGS).optional(),
     separator: z.string().optional(),
   }
